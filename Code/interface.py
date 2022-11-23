f60a40dbaf5a06c143fe5e2931fa72acbec06fac
Ashley Ung <62251927+ung8005@users.noreply.github.com>
2022-11-23 13:47:03 -0800

    need to convert hydrophobic n count to a float

--- Code/interface.py
+++ Code/interface.py
@@ -481,8 +481,8 @@ def write_aqueous_composition():
 	
 	f.write ('Aqueous Interface Composition\n')
 	for atom in range ((len(aqueousTypes))):
-		n= (z_type_aqueous == atom).sum() 
-		if not n == 0:											# check if atom was found in the file 
+		n = (z_type_aqueous == atom).sum() 
+		if not n == 0:										# check if atom was found in the file 
 			f.write("\t{} ".format(aqueousTypes[atom])) 
 		
 	f.write ('\nAqueous Interface Average')
@@ -509,15 +509,19 @@ def write_hydrophobic_composition():
 	
 	f.write ('Hydrophobic Interface Composition\n')
 	for atom in range ((len(hydrophobicTypes))):
-		f.write ("\t{} ".format(hydrophobicTypes[atom]))
+		n = (z_type_hydrophobic == atom).sum()
+		if not n == 0:
+			f.write ("\t{} ".format(hydrophobicTypes[atom]))
 		
 	f.write ('\nHydrophobic Interface Average')
 	for i in range(len(hydrophobicTypes)):
-		f.write ('\t{:.1f}'.format(h_comp_ave[i]))
+		if not h_comp_ave[i] == 0: 
+			f.write ('\t{:.1f}'.format(h_comp_ave[i]))
 		
 	f.write ('\nHydrophobic Interface Standard Deviation')
 	for i in range (len(hydrophobicTypes)):
-		f.write ('\t{:.1f}'.format(h_comp_std[i]))
+		if not h_comp_std[i] == 0:
+			f.write ('\t{:.1f}'.format(h_comp_std[i]))
 		
 	f.write ('\nSampleCount = {:d}\n'.format(Analysis_Count))
 	f.close ()
